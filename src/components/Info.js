3738a083ff4727f32a80a920ff6b0254ebd34cc9
Tahik <tomrihak@gmail.com>
2021-12-07 11:25:17 +0100

    add info route, add front page minting message

---
+++ src/components/Info.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/components/Info.css";
+
+function Info() {
+  return (
+    <div className="info-container">
+      <h1 className="info-font-face-magic">Lore</h1>
+      <p className="info-text">Insert some text here</p>
+      <h1 className="info-font-face-magic">Classes</h1>
+      <p className="info-text">Insert some text here</p>
+      <h1 className="info-font-face-magic">About NFTs</h1>
+      <p className="info-text">Insert some text here</p>
+      <h1 className="info-font-face-magic">About this project</h1>
+      <p className="info-text">Insert some text here</p>
+    </div>
+  );
+}
+
+export default Info;
